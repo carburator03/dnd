77c93028ebc6533c2b7a99014fe46a2b07109bd6
carburator03 <carburator03@proton.me>
2024-04-14 13:46:09 +0200

    added all the tables

---
+++ dnd/database/migrations/2024_04_14_114055_create_characters_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('characters', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->boolean('enemy')->default(false);
+        $table->integer('defence')->unsigned()->default(0);
+        $table->integer('strength')->unsigned()->default(0);
+        $table->integer('accuracy')->unsigned()->default(0);
+        $table->integer('magic')->unsigned()->default(0);
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characters');
+    }
+};

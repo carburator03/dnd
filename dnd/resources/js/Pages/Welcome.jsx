abe068019717f6285931af099aea33bfd7e89ba1
gergob <bogdangergo@gmail.com>
2024-04-24 20:57:06 +0200

    menu frissites

--- dnd/resources/js/Pages/Welcome.jsx
+++ dnd/resources/js/Pages/Welcome.jsx
@@ -5,6 +5,7 @@ import { Avatar } from "../Components/ui/avatar";
 import { Label } from "../Components/ui/label";
 import { useState, useEffect } from "react";
 import { ScoreBoard } from "@/Components/ui/score-board";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Welcome = ({ auth, laravelVersion, phpVersion }) => {
     const [totalCharacters, setTotalCharacters] = useState(0);
@@ -34,13 +35,19 @@ const Welcome = ({ auth, laravelVersion, phpVersion }) => {
         <div className="hidden flex-col md:flex justify-center items-center w-full">
             <div className="border-b w-full">
                 <div className="flex h-16 items-center px-4">
-                    <MainNav className="mx-6" />
+                    <ErrorBoundary fallback={<span />}>
+                        <MainNav className="mx-6" isUserLoggedIn={true} />
+                    </ErrorBoundary>
                 </div>
             </div>
 
             <div className="flex-1 space-y-4 p-8 pt-6 w-1/2">
                 <div className="flex items-center justify-between space-y-2">
-                    <ScoreBoard className="w-full" karakterek={totalCharacters} merkozesek={totalContests}  />
+                    <ScoreBoard
+                        className="w-full"
+                        karakterek={totalCharacters}
+                        merkozesek={totalContests}
+                    />
                 </div>
             </div>
         </div>
